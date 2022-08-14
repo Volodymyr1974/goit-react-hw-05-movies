f8dc70f5d0ff081927ef881700d3b492e7541926
Volodymyr Zvarych <Vbzvar@gmail.com>
2022-08-14 15:42:56 +0300

    add Search

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+// import style from './Searchbar.module.css';
+import PropTypes from 'prop-types';
+// import { ImSearch } from 'react-icons/im';
+import Notiflix from 'notiflix';
+
+
+
+export default function SearchBar({ onSubmit }) {
+    const [qwery, setQwery] = useState('');
+
+    const searchInput = (e) => {
+        setQwery(e.currentTarget.value.toLowerCase());
+    };
+    const searchSubmit = (e) => {
+        e.preventDefault();
+        if (qwery.trim() === '') {
+            return Notiflix.Notify.failure('Вибачте, поле пошуку не заповнено. Введіть запит для пошуку.');
+        }
+
+        onSubmit(qwery);
+    };
+    return (
+        <form
+            // className={style.SearchForm}
+            onSubmit={searchSubmit}>
+
+
+            <input
+                // className={style.SearchForm_input}
+                type="text"
+                autoComplete="off"
+                autoFocus
+                onChange={searchInput}
+
+            />
+            <button type="submit"
+            // className={style.SearchForm_button}
+            >
+
+                <span
+                // className={style.button_label}
+                >Search</span>
+            </button>
+        </form>
+
+    );
+
+};
+SearchBar.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+};
--- src/views/MoviesView.jsx
+++ src/views/MoviesView.jsx
@@ -1,12 +1,54 @@
+import SearchBar from "components/SearchBar/SearchBar";
+import { useState, useEffect } from 'react';
+import * as serviceApi from '../services/Api';
+import { useSearchParams } from 'react-router-dom';
+import Notiflix from 'notiflix';
+import { Link } from 'react-router-dom';
+
+
+
 export default function MoviesView() {
+    const [foundMovies, setFoundMovies] = useState([]);
+    const [search, setSearch] = useSearchParams();
+    const query = search.get('query') ?? '';
+
+    useEffect(() => {
+        async function searchMovie() {
+            if (query === '') {
+                return;
+            }
+            const { results } = await serviceApi.getSearchMovies(query)
+            console.log(results);
+
+            setFoundMovies(results)
+            if (results.length === 0) {
+                return Notiflix.Notify.failure(
+                    'Sorry, there are no movies matching your search query. Please try again.'
+                );
+            }
+        };
+
+        searchMovie();
+    }, [query]);
+
+    console.log(foundMovies);
+
+    const searchQuery = data => {
+        setSearch({ query: data.toLowerCase().trim() });
+    };
     return (
-        <ul>
-            <>
-                <h1>     6767676767</h1>
-            </>
-            <li>11</li>
-            <li>22</li><li>33</li>
-        </ul>
+        <>
+            <SearchBar value={query} onSubmit={searchQuery} />
+            {foundMovies &&
+                <ul>
+                    {foundMovies.map(movie => (
+                        <li key={movie.id}>
+                            <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+                        </li>
+                    ))}
+                </ul>}
+
+        </>
 
 
 
